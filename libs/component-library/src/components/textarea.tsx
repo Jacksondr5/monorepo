a4819a8c80ebe0aa13c85b1c325c51ea317525e6
Jackson Miller <jackson@jackson.codes>
2025-04-22 21:57:23 -0400

    add component-library (#15)
    * add component-library
    
    * fixes
    
    * finalize

---
+++ libs/component-library/src/components/textarea.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+import { cn } from "~/lib/utils";
+
+const Textarea = React.forwardRef<
+  HTMLTextAreaElement,
+  React.ComponentProps<"textarea">
+>(({ className, ...props }, ref) => {
+  return (
+    <textarea
+      className={cn(
+        "field-sizing-content border-olive-7 shadow-xs placeholder:text-olive-11 focus-visible:ring-olive-7 flex min-h-[60px] w-full resize-none rounded-md border bg-transparent px-3 py-2 text-base focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        className,
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Textarea.displayName = "Textarea";
+
+export { Textarea };
