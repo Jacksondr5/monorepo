bcd514112b457e681ce67dde4ec990fe254838b7
Jackson Miller <jackson@jackson.codes>
2025-04-22 21:44:41 -0400

    fixes

--- libs/component-library/src/components/input.tsx
+++ libs/component-library/src/components/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-olive-7 bg-transparent px-3 py-1 text-base shadow-xs transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-olive-11 placeholder:text-olive-11 focus-visible:ring-1 focus-visible:ring-olive-7 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "border-olive-7 shadow-xs file:text-olive-11 placeholder:text-olive-11 focus-visible:ring-olive-7 flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className,
         )}
         ref={ref}
