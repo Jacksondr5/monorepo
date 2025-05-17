da330cfe237256371bf185bc0bd708765efa7009
Jackson Miller <jackson@jackson.codes>
2025-05-16 23:19:49 -0400

    update storybook config and make it global (#61)
    * update storybook config and make it global
    
    * format
    
    * fix
    
    * lockfile
    
    * format
    
    * temp
    
    * finish
    
    * cache
    
    * undo nodenext

--- libs/chromatic/.storybook/preview.ts
+++ libs/chromatic/.storybook/preview.ts
@@ -1,1 +1,7 @@
-import "../src/globals.css";
+import { previewConfig } from "@j5/component-library";
+import "../src/globals.css"; // Keep chromatic specific global styles if any
+
+// Re-export the shared configuration
+export const parameters = previewConfig.parameters;
+export const decorators = previewConfig.decorators;
+export const globalTypes = previewConfig.globalTypes;
