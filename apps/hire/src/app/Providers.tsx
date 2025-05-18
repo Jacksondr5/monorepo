99e20071e90fb9b77ae2baad556048c8f2f9de67
Jackson Miller <jackson@jackson.codes>
2025-05-17 22:57:08 -0400

    add

---
+++ apps/hire/src/app/Providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+import { env } from "../env";
+import { useAuth } from "@clerk/nextjs";
+
+const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL as string);
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
