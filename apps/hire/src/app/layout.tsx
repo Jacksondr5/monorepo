01405358257bd628c4611dce96d9770190e6b644
Jackson Miller <jackson@jackson.codes>
2025-05-17 22:56:57 -0400

    refactor the layout

--- apps/hire/src/app/layout.tsx
+++ apps/hire/src/app/layout.tsx
@@ -1,22 +1,14 @@
-"use client";
-
 import "~/styles/global.css";
 
 import { GeistSans } from "geist/font/sans";
-// import { type Metadata } from "next";
-import { ConvexClientProvider } from "./ConvexClientProvider";
-import { ClerkProvider, useAuth } from "@clerk/nextjs";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { ConvexReactClient } from "convex/react";
-import { env } from "../env";
+import { type Metadata } from "next";
+import { Providers } from "./Providers";
 
-// export const metadata: Metadata = {
-//   title: "Coming Soon",
-//   description: "This app is still under construction",
-//   icons: [{ rel: "icon", url: "/favicon.ico" }],
-// };
-
-const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL as string);
+export const metadata: Metadata = {
+  title: "Coming Soon",
+  description: "This app is still under construction",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
 
 export default function RootLayout({
   children,
@@ -24,11 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <ClerkProvider>
-          <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
-            <ConvexClientProvider>{children}</ConvexClientProvider>
-          </ConvexProviderWithClerk>
-        </ClerkProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
