574ed5aa0661041c22ad992aee185aaf63857ae7
Jackson Miller <jackson@jackson.codes>
2025-07-12 18:19:56 -0400

    pre-work

--- apps/todo/src/components/Task.stories.tsx
+++ apps/todo/src/components/Task.stories.tsx
@@ -1,5 +1,6 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { userEvent, within } from "@storybook/test";
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { userEvent, within } from "storybook/test";
+import React from "react";
 
 import { TaskView, type TaskViewProps } from "./Task";
 
