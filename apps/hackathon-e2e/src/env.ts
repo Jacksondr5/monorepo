93268a231dfa5cf3a878ae25f489573c1b5a4bba
Jackson Miller <jackson@jackson.codes>
2025-07-26 14:13:19 -0400

    init

--- apps/hackathon-e2e/src/env.ts
+++ apps/hackathon-e2e/src/env.ts
@@ -1,7 +1,25 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 import "dotenv/config";
+import DopplerSDK, { type SecretsListResponse } from "@dopplerhq/node-sdk";
 
+const processEnv = createEnv({
+  server: {
+    DOPPLER_TOKEN: z.string().min(1),
+    DOPPLER_ENV: z.string().min(1),
+  },
+  runtimeEnv: process.env,
+});
+
+type SecretGroup = Required<SecretsListResponse>["secrets"];
+type Secret = Required<SecretGroup["USER"]>;
+
+const doppler = new DopplerSDK({ accessToken: processEnv.DOPPLER_TOKEN });
+const result = await doppler.secrets.list("ci", processEnv.DOPPLER_ENV);
+const secrets = result.secrets as Record<string, Secret> | undefined;
+if (!secrets) {
+  throw new Error("No secrets found");
+}
 export const env = createEnv({
   server: {
     BASE_URL: z.string().url(),
@@ -10,8 +28,6 @@ export const env = createEnv({
     PLAYWRIGHT_USER_EMAIL: z.string().email(),
     PLAYWRIGHT_USER_PASSWORD: z.string().min(1),
   },
-  client: {},
-  clientPrefix: "",
   runtimeEnv: {
     BASE_URL: process.env.BASE_URL,
     CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
