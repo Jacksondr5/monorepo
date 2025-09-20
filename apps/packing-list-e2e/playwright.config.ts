7581d9dae8d7061050760dff12dba74579e30b83
Jackson Miller <jackson@jackson.codes>
2025-09-20 16:21:33 -0400

    try 3 different things

--- apps/packing-list-e2e/playwright.config.ts
+++ apps/packing-list-e2e/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 import { nxE2EPreset } from "@nx/playwright/preset";
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import { getBaseURL } from "../../tools/shared/src";
+import { getBaseURL } from "../../tools/shared/src/index.js";
 
 const baseURL = getBaseURL(import.meta.dirname, "packing-list");
 
