a65c7165dabeba026715af7b16ed02c9929031ce
Jackson Miller <jackson@jackson.codes>
2026-01-18 16:44:36 -0500

    try just doing parts of vercel-build-deploy

--- tools/vercel/src/executors/build-deploy/executor.ts
+++ tools/vercel/src/executors/build-deploy/executor.ts
@@ -69,44 +69,44 @@ export default async function buildExecutor(
   }
 
   // Run deploy command
-  const deployResult = await run(
-    `pnpm vercel --prebuilt --archive=tgz --yes --token ${vercelKey}`,
-    {
-      cwd: context.root,
-      env: {
-        ...process.env,
-      },
-    },
-  );
-  if (deployResult.code !== 0) {
-    throw logAndCreateError("vercel deploy failed");
-  }
+  // const deployResult = await run(
+  //   `pnpm vercel --prebuilt --archive=tgz --yes --token ${vercelKey}`,
+  //   {
+  //     cwd: context.root,
+  //     env: {
+  //       ...process.env,
+  //     },
+  //   },
+  // );
+  // if (deployResult.code !== 0) {
+  //   throw logAndCreateError("vercel deploy failed");
+  // }
 
-  console.info("Deployed successfully");
+  // console.info("Deployed successfully");
 
-  const vercel = new Vercel({ bearerToken: vercelKey });
-  console.info(`Getting deployments for project: ${project}`);
-  const recentDeployments = (
-    await vercel.deployments.getDeployments({
-      app: project,
-      sha: commitSha,
-      state: "READY",
-    })
-  ).deployments.sort((a, b) => b.created - a.created);
-  console.info(`Recent deployments: ${JSON.stringify(recentDeployments)}`);
-  if (recentDeployments.length === 0) {
-    throw logAndCreateError("No deployments found");
-  }
-  if (recentDeployments.length > 1) {
-    throw logAndCreateError("Multiple deployments found");
-  }
-  const deployment = recentDeployments[0];
-  console.info(`Deployment: ${JSON.stringify(deployment)}`);
-  const deploymentUrl = deployment.url;
-  console.info(`Deployment URL: ${deploymentUrl}`);
+  // const vercel = new Vercel({ bearerToken: vercelKey });
+  // console.info(`Getting deployments for project: ${project}`);
+  // const recentDeployments = (
+  //   await vercel.deployments.getDeployments({
+  //     app: project,
+  //     sha: commitSha,
+  //     state: "READY",
+  //   })
+  // ).deployments.sort((a, b) => b.created - a.created);
+  // console.info(`Recent deployments: ${JSON.stringify(recentDeployments)}`);
+  // if (recentDeployments.length === 0) {
+  //   throw logAndCreateError("No deployments found");
+  // }
+  // if (recentDeployments.length > 1) {
+  //   throw logAndCreateError("Multiple deployments found");
+  // }
+  // const deployment = recentDeployments[0];
+  // console.info(`Deployment: ${JSON.stringify(deployment)}`);
+  // const deploymentUrl = deployment.url;
+  // console.info(`Deployment URL: ${deploymentUrl}`);
 
-  console.info(`Writing deployment URL to vercel-urls for project`);
-  writeWorkspaceVercelUrl(context.root, project, deploymentUrl);
-  console.info(`Deployment URL written for project ${project}`);
+  // console.info(`Writing deployment URL to vercel-urls for project`);
+  // writeWorkspaceVercelUrl(context.root, project, deploymentUrl);
+  // console.info(`Deployment URL written for project ${project}`);
   return { success: true };
 }
