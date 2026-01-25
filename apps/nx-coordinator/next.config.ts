3c05ec2be8eb91c689cc3cabd18108f2a17143dc
Jackson Miller <jackson@jackson.codes>
2026-01-25 17:28:02 -0500

    remove auth from nx-coordinator (#174)

--- apps/nx-coordinator/next.config.ts
+++ apps/nx-coordinator/next.config.ts
@@ -6,14 +6,6 @@ const nextConfig = {
     svgr: false,
   },
   transpilePackages: ["convex-helpers"],
-  images: {
-    remotePatterns: [
-      {
-        hostname: "img.clerk.com",
-        protocol: "https",
-      },
-    ],
-  },
 } satisfies NextConfig;
 
 const plugins = [withNx];
--- apps/nx-coordinator/src/app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function SignUpPage() {
-  return (
-    <div className="bg-slate-1 flex min-h-screen items-center justify-center">
-      <SignUp />
-    </div>
-  );
-}
